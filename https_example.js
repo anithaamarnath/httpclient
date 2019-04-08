4b2a6cfdc6b3df9da9cb5690270a91d7ee6239c7
anithaamarnath <anithaamarnath04@gmail.com>
2019-04-08 18:21:55 +0000

    the second

--- https_example.js
+++ https_example.js
@@ -1,14 +1,15 @@
-// var https = require('https');
-// console.log('I did it!');
+var https = require('https');
+console.log('I did it!');
 var options = {
   host: 'www.example.org'.
-  path: '/'
+  path: '../..'
 };
 
 
 // called by https when the request is made.
- var callback = function() {
+ var callback = function(response) {
   console.log(" I'm about to male the request!");
+  console.log('Response: ' response);
 
  }
  console.log("I;m about to make the request!");
